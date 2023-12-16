957432d0a47ad2bedc7f881d1ed8c0beec172848
Keiran <80461252+keiran0@users.noreply.github.com>
2023-12-16 12:44:06 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/frontEndMentor_rest-countries-api/",
+  base:"https://keiran0.github.io/frontendMentor_rest-countries-api/",
   plugins: [react()]
 })
